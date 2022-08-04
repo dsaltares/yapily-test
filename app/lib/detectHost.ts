8e1373187b60547a79e879aac44c5e643b87a0b9
David Saltares <david.saltares@gmail.com>
2022-08-04 21:09:51 +0300

    fix: host detection in prod

--- app/lib/detectHost.ts
+++ app/lib/detectHost.ts
@@ -2,7 +2,7 @@ import { NextApiRequest } from "next";
 
 const detectHost = (req: NextApiRequest) => {
   const forwardedHost = req.headers["x-forwarded-host"] ?? req.headers.host;
-  if (process.env.VERCEL) return forwardedHost
+  if (process.env.VERCEL) return `https://${forwardedHost}`;
   return process.env.HOST ?? 'http://localhost:3000';
 };
 
