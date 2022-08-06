d61451671d0dee79e3db4022fd27bd44c1e47732
David Saltares <david.saltares@gmail.com>
2022-08-06 16:07:34 +0300

    feat: add money emoji to favicon and header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Header = () => (
   <header className="w-full py-6 text-4xl font-bold bg-primary text-secondary flex justify-center">
     <Link href="/">
-      <a>Yapily Banking App</a>
+      <a>💸 Yapily Banking App 💸</a>
     </Link>
   </header>
 );
