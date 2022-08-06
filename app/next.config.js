5dc9c40f269a6cc6a3bd375b43b1739bccd357c9
David Saltares <david.saltares@gmail.com>
2022-08-06 13:00:49 +0300

    chore: set page extensions to .page, .next and .route for test co-location

--- app/next.config.js
+++ app/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['page.ts', 'page.tsx', 'next.tsx', 'route.ts'],
   reactStrictMode: true,
   swcMinify: true,
   images: {
