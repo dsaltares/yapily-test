f71b3719ab41598390ce3634b9f15ca8bbf44ae1
David Saltares <david.saltares@gmail.com>
2022-08-06 09:28:48 +0300

    feat: header is a link to the home page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,10 @@
+import Link from 'next/link';
+
 const Header = () => (
   <header className="w-full py-6 text-4xl font-bold bg-primary text-secondary flex justify-center">
-    Yapily Banking App
+    <Link href="/">
+      <a>Yapily Banking App</a>
+    </Link>
   </header>
 );
 
